06eef91dd01a6dcc17850019180b31b187dec8b7
ea-arnob-07 <eaarnob178@gmail.com>
2026-08-09 16:22:21 +0600

    Fix static files loading on Vercel

--- friendhub/settings.py
+++ friendhub/settings.py
@@ -93,7 +93,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
     "staticfiles": {
